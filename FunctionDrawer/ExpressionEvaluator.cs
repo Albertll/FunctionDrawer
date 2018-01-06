a3bab3a6a8684c5d5331d53c7579623b2a2a4542
Albert Leśniak <albertl@vp.pl>
2018-01-06 15:22:43 +0100

    v0.1

---
+++ FunctionDrawer/ExpressionEvaluator.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FunctionDrawer
+{
+    public class ExpressionEvaluator
+    {
+        private Stack<string> _stack;
+        private IList<string> _rnp;
+        private StringBuilder _temp;
+        private readonly string[] _funcs = { "+", "-", "*", "/", "^", "sin", "cos", "tan", "neg" };
+
+        public IOperation Evaluate(string text)
+        {
+            MakeRnp(text);
+
+            var operationStack = new Stack<Operation>();
+
+            if (_rnp.Count == 0)
+                throw new Exception("Zero arguments");
+            
+            foreach (var value in _rnp)
+            {
+                if (IsNumber(value))
+                {
+                    operationStack.Push(new Value(double.Parse(value)));
+                    continue;
+                }
+
+                if (value.Equals("x"))
+                {
+                    operationStack.Push(new X());
+                    continue;
+                }
+
+                if (operationStack.Count == 0)
+                    throw new ArgumentException();
+
+                var right = operationStack.Pop();
+                switch (value)
+                {
+                    case "sin":
+                        operationStack.Push(new Sinus(right));
+                        continue;
+                    case "cos":
+                        operationStack.Push(new Cosinus(right));
+                        continue;
+                    case "tan":
+                        operationStack.Push(new Tangens(right));
+                        continue;
+                    case "neg":
+                        operationStack.Push(new Negative(right));
+                        continue;
+                }
+
+                //if (value.Equals("-") && (i - 2 < 0 || funcs.Contains(_rnp[i-2])))
+                //{
+                //    operationStack.Push(new Negative(right));
+                //    continue;
+                //}
+                if (operationStack.Count == 0)
+                    throw new IndexOutOfRangeException();
+
+                Operation left = operationStack.Pop();
+                switch (value)
+                {
+                    case "+":
+                        operationStack.Push(new Addition(left, right));
+                        continue;
+                    case "-":
+                        operationStack.Push(new Subtraction(left, right));
+                        continue;
+                    case "*":
+                        operationStack.Push(new Multiplication(left, right));
+                        continue;
+                    case "/":
+                        operationStack.Push(new Division(left, right));
+                        continue;
+                    case "^":
+                        operationStack.Push(new Power(left, right));
+                        continue;
+                }
+            }
+
+            if (operationStack.Count > 1)
+                throw new Exception("Too many");
+            return operationStack.Pop();
+        }
+
+        private void MakeRnp(string text)
+        {
+            text = Regex.Replace(text, @"\s+", ""); //spacje
+            text = Regex.Replace(text, @"(?<=[^\d\)]|^)\-(\d+)", "neg($1)");
+            text = Regex.Replace(text, @"(?<=\d)\(", "*(");
+            text = Regex.Replace(text, @"\)(?=\d)", ")*");
+            text = Regex.Replace(text, @"\w+\(\w+\)=(.*)", "$1");
+
+            _stack = new Stack<string>();
+            _temp = new StringBuilder();
+            _rnp = new List<string>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var current = text[i];
+                var next = i + 1 < text.Length ? text[i + 1] : '\0';
+
+                if (current.Equals('x'))
+                {
+                    _rnp.Add("x");
+                    continue;
+                }
+
+                if (EvaluateNumber(current, next))
+                    continue;
+
+                if (EvaluateFunction(current, next))
+                    continue;
+
+                if (current.Equals('('))
+                    _stack.Push("(");
+
+                if (current.Equals(')'))
+                {
+                    while (_stack.Count != 0 && !_stack.Peek().Equals("("))
+                        _rnp.Add(_stack.Pop());
+
+                    if (_stack.Count == 0 || !_stack.Peek().Equals("("))
+                        throw new NotEnoughBrackets();
+
+                    _stack.Pop();
+                }
+            }
+
+            while (_stack.Count != 0)
+            {
+                string op = _stack.Pop();
+
+                if (op.Equals("("))
+                    throw new NotEnoughBrackets();
+
+                if (!_funcs.Contains(op))
+                    throw new FormatException();
+
+                _rnp.Add(op);
+            }
+        }
+
+        public class NotEnoughBrackets : ArithmeticException { }
+
+        private bool EvaluateNumber(char c, char next)
+        {
+            if (char.IsDigit(c) && (_temp.Length == 0 || IsNumber(_temp.ToString())))
+                _temp.Append(c);
+
+            if (IsNumber(_temp.ToString()) && (c.Equals('.') || c.Equals(',')))
+                _temp.Append(c);
+
+            if (!IsNumber(_temp.ToString()) && (char.IsDigit(next) || next.Equals('.') || next.Equals(',')))
+                return false;
+
+            if (char.IsDigit(next) || (IsNumber(_temp.ToString()) && next.Equals('.') || next.Equals(',')))
+                return true;
+
+            if ((_temp.Length != 0 && IsNumber(_temp.ToString())))
+            {
+                _rnp.Add(_temp.ToString());
+                _temp.Clear();
+            }
+
+            return false;
+        }
+
+        private bool EvaluateFunction(char c, char next)
+        {
+            if (char.IsLetter(c) || _funcs.Contains(c.ToString()))
+                _temp.Append(c);
+
+            if (_funcs.Contains(_temp.ToString()) || !char.IsLetter(next) && _temp.Length != 0 && _funcs.Contains(_temp.ToString()))
+            {
+                string op = _temp.ToString();
+                _temp.Clear();
+
+                while (_stack.Count != 0 && !HasHigherPrior(op, _stack.Peek()))
+                    _rnp.Add(_stack.Pop());
+
+                _stack.Push(op);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool HasHigherPrior(string op1, string op2) => GetPrior(op1) > GetPrior(op2);
+
+        private int GetPrior(string op)
+        {
+            switch (op)
+            {
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                    return 2;
+                case "^":
+                    return 3;
+                case "sin":
+                case "cos":
+                case "tan":
+                    return 4;
+                case "neg":
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+
+        private static bool IsNumber(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return false;
+
+            bool dot = false;
+
+            foreach (char c in text)
+                if (!char.IsDigit(c))
+                {
+                    if (!dot && (c.Equals('.') || c.Equals(',')))
+                            dot = true;
+                    else
+                        return false;
+                }
+
+            return true;
+        }
+    }
+}
---
+++ FunctionDrawer/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace FunctionDrawer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.functionInputTextBox = new System.Windows.Forms.TextBox();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // functionInputTextBox
+            // 
+            this.functionInputTextBox.Location = new System.Drawing.Point(51, 6);
+            this.functionInputTextBox.Name = "functionInputTextBox";
+            this.functionInputTextBox.Size = new System.Drawing.Size(169, 20);
+            this.functionInputTextBox.TabIndex = 0;
+            this.functionInputTextBox.Text = "x";
+            this.functionInputTextBox.TextChanged += new System.EventHandler(this.functionInputTextBox_TextChanged);
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(12, 39);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(38, 13);
+            this.xLabel.TabIndex = 2;
+            this.xLabel.Text = "xLabel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "f(x) = ";
+            // 
+            // yLabel
+            // 
+            this.yLabel.AutoSize = true;
+            this.yLabel.Location = new System.Drawing.Point(12, 62);
+            this.yLabel.Name = "yLabel";
+            this.yLabel.Size = new System.Drawing.Size(38, 13);
+            this.yLabel.TabIndex = 7;
+            this.yLabel.Text = "yLabel";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 529);
+            this.Controls.Add(this.yLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.functionInputTextBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
+            this.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.OnMouseWheel);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox functionInputTextBox;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label yLabel;
+    }
+}
+
